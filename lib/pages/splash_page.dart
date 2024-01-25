2eb53ee811315b4929418e225476efc5af1a7bff
dion <juniordion7@gmail.com>
2024-01-26 04:23:55 +0800

    footer splash page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -6,54 +6,63 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(top: 50.0, left: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              "assets/images/logo.png",
-              width: 70,
-              height: 70,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Find Cozy House\nto Stay and Happy",
-              style: blackTextStyle.copyWith(fontSize: 24),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Stop membuang banyak waktu\npada tempat yang tidak habitable",
-              style: greyTextStyle.copyWith(
-                fontSize: 16,
+          bottom: false,
+          child: Stack(
+            children: [
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset('assets/images/splash_bg.png'),
               ),
-            ),
-            SizedBox(
-              height: 51,
-            ),
-            Container(
-              width: 210,
-              height: 60,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    primary: purpleColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(17),
-                    )),
-                child: Text(
-                  "Explore Now",
-                  style: whiteTextStyle.copyWith(fontSize: 18),
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0, left: 30.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      "assets/images/logo.png",
+                      width: 70,
+                      height: 70,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Find Cozy House\nto Stay and Happy",
+                      style: blackTextStyle.copyWith(fontSize: 24),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Stop membuang banyak waktu\npada tempat yang tidak habitable",
+                      style: greyTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 51,
+                    ),
+                    Container(
+                      width: 210,
+                      height: 60,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            primary: purpleColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(17),
+                            )),
+                        child: Text(
+                          "Explore Now",
+                          style: whiteTextStyle.copyWith(fontSize: 18),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
-      )),
+            ],
+          )),
     );
   }
 }
